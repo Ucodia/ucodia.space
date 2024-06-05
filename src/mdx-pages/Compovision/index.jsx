e32ee1f35ce4592201a5e713156b6c4bfe80fb44
Lionel Ringenbach <ucodia@live.com>
2024-06-04 18:34:27 -0700

    Add MDX global layout

--- src/mdx-pages/Compovision/index.jsx
+++ src/mdx-pages/Compovision/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
 import MDXPage from "./index.mdx";
+import MDXLayout from "../MDXLayout";
 
 const components = {};
 
 const Compovision = () => (
   <MDXProvider components={components}>
-    <article className="prose">
+    <MDXLayout>
       <MDXPage />
-    </article>
+    </MDXLayout>
   </MDXProvider>
 );
 
